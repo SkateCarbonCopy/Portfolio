4fa43521448eab2a0e7bcc7afcdacf7300f7181a
Colin <crgastelle@gmail.com>
2022-03-21 21:36:07 -0600

    adding minor adjustments after removing old css library

--- src/components/GalleryContainer/GalleryContainer.js
+++ src/components/GalleryContainer/GalleryContainer.js
@@ -4,19 +4,19 @@ import GalleryObject from '../GalleryObject/GalleryObject';
 class GalleryContainer extends React.Component {
     render() {
         return (
-            <div className="elegant-color pt-5">
+            <div className="pt-5">
                 <div className="container mt-5">
                     <section id="portfolio">
-                        <h3 className="font-weight-bold text-center pb-2 white-text">Portfolio</h3>
+                        <h3 className="text-center pb-2 white-text">Portfolio</h3>
                         <hr className="w-header my-4" />
                         <p className="lead text-center text-muted pt-2 mb-5 white-text">Below are a few personal projects I've worked on, from character design for an animated series to skateboard graphics.</p>
                     {/*
                     <div className="row">
                         <div className="col-md-12 dark-grey-text d-flex justify-content-center mb-5">
                             <button type="button" className="btn btn-flat btn-lg m-0 px-3 py-1 filter" data-rel="all">All</button>
-                            <p className="font-weight-bold mb-0 px-1 py-1">/</p>
+                            <p className="mb-0 px-1 py-1">/</p>
                             <button type="button" className="btn btn-flat btn-lg m-0 px-3 py-1 filter" data-rel="1">Design</button>
-                            <p className="font-weight-bold mb-0 px-1 py-1">/</p>
+                            <p className="mb-0 px-1 py-1">/</p>
                             <button type="button" className="btn btn-flat btn-lg m-0 px-3 py-1 filter" data-rel="2">Mockup</button>
                         </div>
                     </div>
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -11,20 +11,22 @@ class Landing extends React.Component {
         return(
             <header>
                 <div className="view" style={headerStyles}>
-                    <div className="d-flex justify-content-center align-items-center">
+                    <div className="d-flex h-100 justify-content-center align-items-center">
                         <div className="container">
                             <div className="row">
-                            <div className="col-md-12 white-text text-center">
-                                <h1 className="display-3 mb-0 pt-md-5 pt-5 white-text d-none d-md-block">COLIN
-                                    <span className="indigo-text font-weight-bold">GASTELLE</span>
+                            <div className="col-md-12 text-center" id="introduction">
+                                <h1 className="display-3 mb-0 pt-md-5 pt-5 d-none d-md-block text-glow">
+                                    <span className="text-light">COLIN</span>
+                                    <span className="font-weight-bold">GASTELLE</span>
                                 </h1>
-                                <h1 className="mb-0 pt-md-5 pt-5 white-text d-md-none">COLIN
-                                    <span className="indigo-text font-weight-bold"> GASTELLE</span>
+                                <h1 className="mb-0 pt-md-5 pt-5 d-md-none">
+                                    <span className="text-light">COLIN</span>
+                                    <span className="font-weight-bold"> GASTELLE</span>
                                 </h1>
-                                <h3 className="text-uppercase py-4 white-text font-weight-bold">Web developer &amp; Artist</h3>
+                                <h3 className="text-uppercase text-light py-4 font-weight-bold">Web developer &amp; Artist</h3>
                                 <div>
-                                    <a className="btn btn-indigo btn-lg btn-rounded" href="#aboutme">About me</a>
-                                    <a className="btn btn-light-blue btn-lg btn-rounded" href="#portfolio">Portfolio</a>
+                                    <a className="btn btn-outline-light btn-lg btn-rounded mr-3" href="#aboutme">About me</a>
+                                    <a className="btn btn-outline-light btn-lg btn-rounded" href="#portfolio">Portfolio</a>
                                 </div>
                             </div>
                             </div>
