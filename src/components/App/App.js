0ff6cd220b99b0f8e448578c54fa0d6fc36f40f0
Colin <crgastelle@gmail.com>
2022-03-22 09:09:22 -0600

    test commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,20 +7,6 @@ import GalleryContainer from '../GalleryContainer/GalleryContainer';
 import AboutMe from '../AboutMe/AboutMe';
 import Footer from '../Footer/Footer';
 
-/*
-shuffle = (a) => {
-  var j, x, i;
-  for (i = a.length - 1; i > 0; i--) {
-      j = Math.floor(Math.random() * (i + 1));
-      x = a[i];
-      a[i] = a[j];
-      a[j] = x;
-  }
-  return a;
-}
-*/
-
-
 function App() {
   return (
     <div className="App">
